cebe24b7160f85eda1066383ff7ac704792a994e
Brian Pace <pacebrian0@gmail.com>
2020-10-11 17:44:30 +0200

    Added Frontend angular

--- HealthCheckApp/Startup.cs
+++ HealthCheckApp/Startup.cs
@@ -26,7 +26,11 @@ namespace HealthCheckApp
             {
                 configuration.RootPath = "ClientApp/dist";
             });
-            services.AddHealthChecks();
+            services.AddHealthChecks().AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com",
+                    100))
+                    .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com",
+                    100))
+                    .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-notexist.com", 100));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +56,7 @@ namespace HealthCheckApp
 
             app.UseRouting();
 
-            app.UseHealthChecks("/hc");
+            app.UseHealthChecks("/hc", new CustomHealthCheckOptions());
 
             app.UseEndpoints(endpoints =>
             {
